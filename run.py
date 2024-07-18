dad03a05ad6f6b0a73c9e990f7b38f2df491f2bc
francesco <francesco.pinsone@alcampus.it>
2024-07-18 02:40:29 +0200

    Annotazioni e TODO

--- run.py
+++ run.py
@@ -12,3 +12,22 @@ df = imputate_missing_values(df)
 
 # Rimozione delle colonne con correlazione univoca
 df = remove_columns_with_unique_correlation(df)
+
+
+'''
+Statistiche valori mancanti dopo l'imputazione:
+
+codice_provincia_residenza      28380
+comune_residenza                  135
+codice_provincia_erogazione     28776
+ora_inizio_erogazione           23652
+ora_fine_erogazione             23652
+data_disdetta                  460639
+'''
+
+# TODO: rimuovere colonne relative a 'codice_provincia_residenza' e 'codice_provincia_erogazione' in quanto non utili
+
+# TODO: rimuovere tutti i campioni che hanno una data_disdetta non nulla poiché non significativi dato che la
+#  televisita non è avvenuta ma è stata annullata. In questo modo non ci saranno valori mancanti per 'ora_inizio_erogazione' e 'ora_fine_erogazione'
+
+# TODO: rimuovere colonne relative a 'data_disdetta' in quanto non utili. Dopo il secondo TODO per quella feature ci saranno solo valori mancanti
