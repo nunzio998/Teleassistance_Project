1a9dd34fa269dbebe1bfc65b2017f4f21b6a32ce
francesco <francesco.pinsone@alcampus.it>
2024-07-19 13:29:13 +0200

    feature extraction first draft

---
+++ data_prep/feature_extraction.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from datetime import datetime
+
+
+def extract_durata_televisita(df):
+    # Calcolare la durata della televisita
+    # Assicurarsi che 'ora_inizio_erogazione' e 'ora_fine_erogazione' siano in formato datetime
+    df['ora_inizio_erogazione'] = pd.to_datetime(df['ora_inizio_erogazione'], errors='coerce')
+    df['ora_fine_erogazione'] = pd.to_datetime(df['ora_fine_erogazione'], errors='coerce')
+
+    # Calcolare la durata in minuti
+    df['durata_televisita'] = (df['ora_fine_erogazione'] - df['ora_inizio_erogazione']).dt.total_seconds() / 60
+
+    return df
+
+
+def extract_eta_paziente(df):
+    # Assicurarsi che 'data_nascita' sia in formato datetime
+    df['data_nascita'] = pd.to_datetime(df['data_nascita'], errors='coerce')
+
+    # Calcolare l'età in anni
+    current_date = datetime.now()
+
+    # Funzione per calcolare l'età
+    def calcola_eta(row):
+        birth_date = row['data_nascita']
+        if pd.isnull(birth_date):
+            return None
+        age = current_date.year - birth_date.year - (
+                (current_date.month, current_date.day) < (birth_date.month, birth_date.day))
+        return age
+
+    df['eta_paziente'] = df.apply(calcola_eta, axis=1)
+
+    return df
+
+
+def extract_incremento(df):
+    """
+    Aggiunge una nuova feature al DataFrame: incremento.
+    :param df:
+    :return:
+    """
+    # TODO: Implementare la funzione
+    return df
+
+
+def feature_extraction(df):
+    """
+    Aggiunge nuove features al DataFrame: età del paziente e durata della televisita.
+    :param df: Il DataFrame originale.
+    :return: Il DataFrame con le nuove features.
+    """
+
+    # Calcolare l'età del paziente
+    df = extract_eta_paziente(df)
+
+    # Calcolare la durata della televisita
+    df = extract_durata_televisita(df)
+    return df
