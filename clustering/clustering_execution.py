e5d667668d231e58914ea7717bf33f0f95e077b7
ClaudiaMarano <marano_claudia@libero.it>
2024-09-07 22:27:19 +0200

    Aggiunta commenti al file clustering_execution

--- clustering/clustering_execution.py
+++ clustering/clustering_execution.py
@@ -3,62 +3,66 @@ from sklearn.compose import ColumnTransformer
 from sklearn.cluster import KMeans
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-import pandas as pd
 
 def encoding_features(df):
-    # Definisci le colonne categoriche
-    categorical_features = [
-        'asl_residenza', 'provincia_residenza', 'comune_residenza',
-        'asl_erogazione', 'provincia_erogazione',
-        'struttura_erogazione', 'tipologia_struttura_erogazione', 'tipologia_professionista_sanitario',
-        'sesso'
+
+    """
+    Metodo che effettua l'encoding delle feature categoriche
+    :param df: dataframe
+    :return: df, encoded_features, features_name
+
+    """
+
+    # Definisco le colonne delle feature categoriche
+    categorical_features = ['asl_residenza', 'provincia_residenza', 'comune_residenza',
+        'asl_erogazione', 'provincia_erogazione', 'struttura_erogazione', 'tipologia_struttura_erogazione',
+        'tipologia_professionista_sanitario', 'sesso'
     ]
 
-    # Elimina i dati del 2019
-    df = df[df['year'] != 2019].copy()  # Creiamo una copia esplicita del DataFrame
-    print(f"Lunghezza di df dopo eliminazione del 2019: {len(df)}")
+    df = df[df['year'] != 2019].copy()  # Creo una copia del DataFrame, eliminando i dati del 2019, poichè l'incremento è NaN
 
-    # Resetta l'indice
-    df.reset_index(drop=True, inplace=True)
+    df.reset_index(drop=True, inplace=True)  # Resetta l'indice
 
     # Preprocessing delle feature
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('cat', OneHotEncoder(), categorical_features)
-        ]
-    )
+    preprocessor = ColumnTransformer(transformers=[('cat', OneHotEncoder(), categorical_features)])
+
     # Trasformazione delle feature
     encoded_features = preprocessor.fit_transform(df.drop(['incremento', 'id_prenotazione', 'id_paziente'], axis=1))
 
     # Ottieni i nomi delle feature trasformate
     feature_names = preprocessor.get_feature_names_out()
-    print(f"Lunghezza di encoded_features: {encoded_features.shape[0]}")
 
     return df, encoded_features, feature_names
 
 
 def kmeans_clustering(df, X):
+    """
+    Metodo che effettua il clustering attraverso l'algoritmo k-means
+    :param df:
+    :param X:
+    :return:
+    """
     # Numero di cluster basato sulle categorie di incremento
-    n_clusters = len(df['incremento'].dropna().unique())  # Assicurati di considerare solo valori non NaN
+    n_clusters = len(df['incremento'].dropna().unique())  # Considero solo valori non NaN
     kmeans = KMeans(n_clusters=n_clusters, random_state=42)
 
     # Fit e predizione dei cluster
     cluster_labels = kmeans.fit_predict(X)
 
-    # Assicurati che il numero di righe corrisponda
+    # Controllo che il numero di righe corrisponda
     if len(cluster_labels) != len(df):
         raise ValueError(
             f"Lunghezza delle etichette di cluster ({len(cluster_labels)}) non corrisponde alla lunghezza di df ({len(df)})")
 
-    df.loc[:, 'cluster'] = cluster_labels  # Usa .loc per modificare il DataFrame
-
-    # Verifica la lunghezza delle etichette
-    print(f"Lunghezza delle etichette di cluster: {len(cluster_labels)}")
-    print(f"Lunghezza di df['cluster']: {len(df['cluster'])}")
+    df.loc[:, 'cluster'] = cluster_labels  # Uso .loc per modificare il DataFrame
 
 
 def cluster_plot(df):
+    """
+    Metodo che plotta la distribuzione dei cluster rispetto all'incremento
+    :param df:
+    :return:
+    """
     plt.figure(figsize=(12, 8))
     sns.countplot(x='incremento', hue='cluster', data=df)
     plt.title('Distribuzione dei Cluster rispetto a Incremento')
@@ -66,11 +70,16 @@ def cluster_plot(df):
 
 
 def execute_clustering(df):
-    # Elimina i dati del 2019 e resetta l'indice
+    """
+    Metodo che esegue tutti i metodi del file clustering_execution
+    :param df:
+    :return:
+
+    """
     df, encoded_features, features_name = encoding_features(df)
-    # Esegui PCA per visualizzare l'importanza delle feature
 
     kmeans_clustering(df, encoded_features)
+
     cluster_plot(df)
 
 
