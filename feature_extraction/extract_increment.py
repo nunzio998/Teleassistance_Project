0aabd7f0402675203c9c678d4541823a28d2e61d
Lorenzo-Pagliaricci <lorenzopagliaricci@gmail.com>
2024-09-09 17:48:56 +0200

    Aggiornamento e modifiche file extract_increment

--- feature_extraction/extract_increment.py
+++ feature_extraction/extract_increment.py
@@ -1,54 +1,69 @@
 import pandas as pd
-import io
 
-def conta_televisite_per_mesi(df, anni, mesi, tipologia):
-    """
-    Conta il numero totale di televisite per una tipologia di professionista sanitario specificata,
-    in un intervallo di mesi e anni.
+def dati_da_utilizzare(df):
+    tipologie = df['tipologia_professionista_sanitario'].unique()
+    DaF=pd.read_parquet('datasets/df_aggregato.parquet')
+    # Lista degli intervalli di anni e mesi
+    intervalli_anni_mesi = [
+        (2019, 2020, '1, 2, 3, 4'), (2019, 2020, '5, 6, 7, 8'), (2019, 2020, '9, 10, 11, 12'),
+        (2020, 2021, '1, 2, 3, 4'), (2020, 2021, '5, 6, 7, 8'), (2020, 2021, '9, 10, 11, 12'),
+        (2021, 2022, '1, 2, 3, 4'), (2021, 2022, '5, 6, 7, 8'), (2021, 2022, '9, 10, 11, 12')
+    ]
+    return tipologie , DaF, intervalli_anni_mesi
 
-    Args:
-    df (pd.DataFrame): Il DataFrame contenente i dati mensili per ogni tipologia e anno.
-    anni (list): Lista degli anni di interesse.
-    mesi (list): Lista dei mesi di interesse.
-    tipologia (str): La tipologia di professionista sanitario di cui calcolare le televisite.
+def get_intervallo_mesi(mese):
+    intervalli_mesi = {
+        (1, 2, 3, 4): '1, 2, 3, 4',
+        (5, 6, 7, 8): '5, 6, 7, 8',
+        (9, 10, 11, 12): '9, 10, 11, 12'
+    }
 
-    Returns:
-    dict: Un dizionario con gli anni come chiavi e il totale delle televisite per quell'anno come valori.
-    """
-    # Filtrare il DataFrame per includere solo gli anni e i mesi specificati
-    df_filtered = df[(df['anno'].isin(anni)) & (df['mese'].isin(mesi))]
+    for mesi, intervallo in intervalli_mesi.items():
+        if mese in mesi:
+            return intervallo
 
-    # Filtrare ulteriormente per la tipologia di professionista sanitario specificata
-    df_professionista = df_filtered[df_filtered['tipologia_professionista_sanitario'].str.lower() == tipologia.lower()]
+    return "Intervallo non trovato"
 
-    # Calcolare la somma delle televisite per ciascun anno
-    return {anno: df_professionista[df_professionista['anno'] == anno]['conteggio'].sum() for anno in anni}
+def somma_per_intervallo_mesi(df, tipologie):
+    # Aggiungi una colonna per l'intervallo di mesi
+    df['intervallo_mesi'] = df['mese'].apply(get_intervallo_mesi)
 
-def calcola_incremento_percentuale(df, anno1, anno2, mesi, tipologia):
-    """
-    Calcola l'incremento percentuale delle televisite tra due anni per una tipologia di professionista sanitario
-    specificata in un intervallo di mesi.
+    # Filtra i dati per le tipologie specificate
+    df_filtrato = df[df['tipologia_professionista_sanitario'].isin(tipologie)]
 
-    Args:
-    df (pd.DataFrame): Il DataFrame contenente i dati mensili per ogni tipologia e anno.
-    anno1 (int): Il primo anno per il confronto.
-    anno2 (int): Il secondo anno per il confronto.
-    mesi (list): Lista dei mesi di interesse.
-    tipologia (str): La tipologia di professionista sanitario di cui calcolare l'incremento percentuale.
+    # Raggruppa per tipologia, anno e intervallo di mesi, e somma i conteggi
+    risultato = df_filtrato.groupby(['tipologia_professionista_sanitario', 'anno', 'intervallo_mesi'])[
+        'conteggio'].sum().reset_index()
 
-    Returns:
-    float: L'incremento percentuale tra i due anni per la tipologia specificata.
-    """
-    # Ottenere i conteggi per gli anni specificati
-    conteggi = conta_televisite_per_mesi(df, [anno1, anno2], mesi, tipologia)
+    return risultato
 
-    # Calcolare il conteggio per i due anni
-    conteggio_anno1 = conteggi.get(anno1, 0)
-    conteggio_anno2 = conteggi.get(anno2, 0)
+def calcola_incremento(df,intervalli_anni_mesi):
+    # Crea una tabella pivot per avere anni come colonne
+    df_pivot = df.pivot_table(index=['tipologia_professionista_sanitario', 'intervallo_mesi'], columns='anno',
+                              values='conteggio').reset_index()
 
-    # Calcolare l'incremento percentuale
-    incremento = ((conteggio_anno2 - conteggio_anno1) / conteggio_anno1) * 100 if conteggio_anno1 > 0 else float('inf')
-    return incremento
+    def incremento_percentuale(row, anno1, anno2):
+        conteggio_anno1 = row.get(anno1, 0)
+        conteggio_anno2 = row.get(anno2, 0)
+        return ((conteggio_anno2 - conteggio_anno1) / conteggio_anno1) * 100 if conteggio_anno1 > 0 else float('inf')
+
+    risultati = []
+    for (anno1, anno2, intervallo) in intervalli_anni_mesi:
+        df_intervallo = df_pivot[df_pivot['intervallo_mesi'] == intervallo]
+        for _, row in df_intervallo.iterrows():
+            incremento = incremento_percentuale(row, anno1, anno2)
+            classificazione=classifica_incremento(incremento)
+            risultati.append({
+                'tipologia_professionista_sanitario': row['tipologia_professionista_sanitario'],
+                'anno': f"{anno1}-{anno2}",
+                'intervallo_mesi': intervallo,
+                'incremento_percentuale': incremento,
+                'incremento':classificazione
+            })
+
+    risultato_con_incremento = pd.DataFrame(risultati)
+
+    return risultato_con_incremento
 
 
 def classifica_incremento(percentuale):
@@ -72,127 +87,64 @@ def classifica_incremento(percentuale):
         return 'media'  # Per valori tra 1% e 50%, assumiamo 'media'.
 
 
-def salva_incremento_percentuale_per_intervalli(df, tipologie, intervalli_anni_mesi, output_file):
-    """
-    Calcola e salva l'incremento percentuale delle televisite per vari intervalli di mesi e anni,
-    per ogni tipologia di professionista sanitario, in un file Parquet.
+def estendi_incremento(df):
+    # Crea un DataFrame vuoto per i dati estesi
+    dati_estesi = []
 
-    Args:
-    df (pd.DataFrame): Il DataFrame contenente i dati mensili per ogni tipologia e anno.
-    tipologie (list): Lista delle tipologie di professionisti sanitari di interesse.
-    intervalli_anni_mesi (list): Lista degli intervalli di anni e mesi per cui calcolare l'incremento percentuale.
-    output_file (str): Il percorso del file Parquet in cui salvare i risultati.
-    """
-    risultati = []
-    for tipologia in tipologie:
-        for (anno1, anno2, mesi) in intervalli_anni_mesi:
-            # Calcolare l'incremento percentuale
-            incremento = calcola_incremento_percentuale(df, anno1, anno2, mesi, tipologia)
-            classificazione = classifica_incremento(incremento)
-            risultati.append({
-                'tipologia': tipologia,
-                'mesi': f"{mesi[0]},{mesi[-1]}",
-                'anno': f"{anno1},{anno2}",
-                'percentuale': incremento,
-                'incremento': classificazione
+    # Itera su ogni riga del DataFrame esistente
+    for _, row in df.iterrows():
+        anno_intervallo = row['anno'].split('-')
+        anno_fine = int(anno_intervallo[1])  # Prendi solo l'anno finale
+
+        # Ottieni l'intervallo di mesi come lista
+        mesi = row['intervallo_mesi'].split(', ')
+
+        # Aggiungi una riga per ogni mese dell'intervallo
+        for mese in mesi:
+            dati_estesi.append({
+                'tipologia_professionista_sanitario': row['tipologia_professionista_sanitario'],
+                'year': anno_fine,
+                'month': int(mese),
+                'incremento_percentuale': row['incremento_percentuale'],
+                'incremento':row['incremento']
             })
 
-    # Creare un DataFrame con i risultati
-    df_risultati = pd.DataFrame(risultati)
+    # Converti la lista di dizionari in un DataFrame
+    df_esteso = pd.DataFrame(dati_estesi)
 
-    # Trasforma il DataFrame in un buffer Parquet senza salvarlo su disco
-    incremento_percentuale_compatto = io.BytesIO()
-    df_risultati.to_parquet(incremento_percentuale_compatto, index=False)
+    # Ordina i dati per tipologia, anno e mese
+    df_esteso = df_esteso.sort_values(by=['tipologia_professionista_sanitario', 'year', 'month']).reset_index(drop=True)
 
-    # Sposta il puntatore all'inizio del buffer
-    incremento_percentuale_compatto.seek(0)
+    return df_esteso
 
-    # Leggi il buffer Parquet in un nuovo DataFrame
-    df_incremento = pd.read_parquet(incremento_percentuale_compatto)
+def unisci_incremento(df_originale, risultato_esteso):
+    # Unisci il DataFrame originale con il risultato esteso, mantenendo tipologia, anno e mese
+    df_unito = pd.merge(df_originale,
+                        risultato_esteso[['tipologia_professionista_sanitario', 'year', 'month', 'incremento']],
+                        on=['tipologia_professionista_sanitario', 'year', 'month'],
+                        how='left')
 
-    # Espandi i valori di 'mesi' per coprire tutti i mesi nel range e prendi solo il secondo anno
-    expanded_rows = []
-    for _, row in df_incremento.iterrows():
-        mesi_range = row['mesi'].split(',')
-        for mese in range(int(mesi_range[0]), int(mesi_range[1]) + 1):
-            anno = int(row['anno'].split(',')[1])  # Prendi solo il secondo anno
-            expanded_row = row.copy()
-            expanded_row['month'] = mese
-            expanded_row['anno'] = anno
-            expanded_rows.append(expanded_row)
+    df_unito.loc[df_unito['year'] == 2019, 'incremento'] = 'costante'
 
-    df_incremento_expanded = pd.DataFrame(expanded_rows)
-
-    # Rinomina la colonna 'anno' in 'year'
-    df_incremento_expanded.rename(columns={'anno': 'year'}, inplace=True)
-
-    # Mantieni solo le colonne necessarie
-    df_incremento_expanded = df_incremento_expanded[['tipologia', 'month', 'year', 'percentuale', 'incremento']]
-
-    # Trasforma il DataFrame in un buffer Parquet senza salvarlo su disco
-    incremento_percentuale_espanso = io.BytesIO()
-    df_incremento_expanded.to_parquet(incremento_percentuale_espanso, index=False)
-
-    # Sposta il puntatore all'inizio del buffer
-    incremento_percentuale_espanso.seek(0)
-
-    # Leggi il buffer Parquet in un nuovo DataFrame
-    df_incremento_percentuale_espanso = pd.read_parquet(incremento_percentuale_espanso)
-
-    # Elimina la colonna 'index' se esiste
-    if 'index' in df_incremento_percentuale_espanso.columns:
-        df_incremento_percentuale_espanso.drop(columns=['index'], inplace=True)
-
-    # Salva il DataFrame espanso in un file Parquet
-    df_incremento_percentuale_espanso.to_parquet(output_file, index=False)
-
-
-def extract_colum_increment(df,k):
-    # Rinomina le colonne per facilitare l'unione
-    k.rename(columns={'tipologia': 'tipologia_professionista_sanitario', 'month': 'mesi'}, inplace=True)
-
-    # Unisci i DataFrame sulle colonne 'tipologia_professionista_sanitario', 'year', e 'mesi'
-    df_merged = pd.merge(df, k[['tipologia_professionista_sanitario', 'year', 'mesi', 'incremento']],
-                         left_on=['tipologia_professionista_sanitario', 'year', 'month'],
-                         right_on=['tipologia_professionista_sanitario', 'year', 'mesi'],
-                         how='left')
-
-    # Rimuovi la colonna 'mesi' che è stata aggiunta durante l'unione
-    df_merged.drop(columns=['mesi'], inplace=True)
-
-    return df_merged
+    return df_unito
 
 def incremento(df):
 
-    # Carica il file df_aggregato.parquet
-    file_path = 'datasets/df_aggregato.parquet'
-    df_aggregato = pd.read_parquet(file_path)
-
+    tipologie, DaF, intervalli_anni_mese = dati_da_utilizzare(df)
     print("Calcolo la variabile incremento...")
+    risultato = somma_per_intervallo_mesi(DaF, tipologie)
+    risultato_con_incremento = calcola_incremento(risultato, intervalli_anni_mese)
+    risultato_esteso = estendi_incremento(risultato_con_incremento)
 
-    # Ottieni tutte le tipologie di professionisti sanitari presenti nel dataset
-    tipologie = df_aggregato['tipologia_professionista_sanitario'].unique()
+    # Salva il DataFrame esteso in un nuovo file
+    risultato_esteso.to_parquet('datasets/df_incremento_percentuale_esteso.parquet', index=False)
+    #print(risultato_esteso.head(500).to_string())
 
-    # Definisci gli intervalli di anni e mesi di interesse
-    intervalli_anni_mesi = [
-        (2019, 2020, [1, 2, 3, 4]),
-        (2019, 2020, [5, 6, 7, 8]),
-        (2019, 2020, [9, 10, 11, 12]),
-        (2020, 2021, [1, 2, 3, 4]),
-        (2020, 2021, [5, 6, 7, 8]),
-        (2020, 2021, [9, 10, 11, 12]),
-        (2021, 2022, [1, 2, 3, 4]),
-        (2021, 2022, [5, 6, 7, 8]),
-        (2021, 2022, [9, 10, 11, 12])
-    ]
+    # Unisci la colonna incremento al DataFrame originale
+    df_finale = unisci_incremento(df, risultato_esteso)
+    tipologie, DaF, intervalli_anni_mese = dati_da_utilizzare(df)
 
-    # Salva l'incremento percentuale per i vari intervalli di mesi e anni per ogni tipologia di professionista sanitario in un file Parquet
-    output_file = 'datasets/incremento_percentuale.parquet'
-    salva_incremento_percentuale_per_intervalli(df_aggregato, tipologie, intervalli_anni_mesi, output_file)
-    L = pd.read_parquet('datasets/incremento_percentuale.parquet')
-    df = extract_colum_increment(df, L)
-
-    return df
+    return df_finale
 
 
 
