c9d4c3566a2a58c457af2a2d488c14c3ca8418b2
francesco <francesco.pinsone@alcampus.it>
2024-09-15 23:02:49 +0200

    refactor

--- run.py
+++ run.py
@@ -1,20 +1,25 @@
 import pandas as pd
 from data_prep.data_cleaning import data_cleaning
-from data_prep.features_selection import feature_selection_execution
+from data_prep.features_selection import feature_selection
 from feature_extraction.features_extraction import feature_extraction
-from data_transformation.data_transformation import data_transformation
 from feature_extraction.extract_increment import incremento
 from clustering.clustering_execution import execute_clustering
 
+# Configura pandas per visualizzare un DataFrame senza limiti di spazio
+pd.set_option('display.max_rows', None)  # Nessun limite sul numero di righe
+pd.set_option('display.max_columns', None)  # Nessun limite sul numero di colonne
+pd.set_option('display.max_colwidth', None)  # Nessun limite sulla larghezza delle colonne
+pd.set_option('display.expand_frame_repr', False)  # Non espande il DataFrame su più righe
+
 # Caricamento del dataset
-file_path = 'datasets/challenge_campus_biomedico_2024.parquet'
+file_path = 'challenge_campus_biomedico_2024.parquet'
 df = pd.read_parquet(file_path)
 
 # STEP 1: Data Cleaning
 df = data_cleaning(df)
 
 # STEP 2: Features Selection
-df = feature_selection_execution(df)
+df = feature_selection(df)
 
 # STEP 3: Feature extraction
 df = feature_extraction(df)
@@ -29,10 +34,9 @@ df, label_encoders, reverse_mapping, numerical_features, categorical_features = 
 df_clustered, cluster_labels, svd_transformed_data = execute_clustering(df, label_encoders, numerical_features,
                                                                         categorical_features, reverse_mapping)
 
-
-
-
-
+# # Visualizzazione del numero di righe e colonne del dataset
+# num_rows, num_columns = df.shape
+# print(f" Dopo il clustering, il DataFrame ha {num_rows} righe e {num_columns} colonne.")
 
 
 
@@ -48,12 +52,6 @@ ora_fine_erogazione             23652
 data_disdetta                  460639
 '''
 
-# TODO: rimuovere colonne relative a 'codice_provincia_residenza' e 'codice_provincia_erogazione' in quanto non utili
-
-# TODO: rimuovere tutti i campioni che hanno una data_disdetta non nulla poiché non significativi dato che la
-#  televisita non è avvenuta ma è stata annullata. In questo modo non ci saranno valori mancanti per 'ora_inizio_erogazione' e 'ora_fine_erogazione'
-
-# TODO: rimuovere colonne relative a 'data_disdetta' in quanto non utili. Dopo il secondo TODO per quella feature ci saranno solo valori mancanti
 
 """
 DOPO TODO:
@@ -96,3 +94,5 @@ La metrica finale è: 0.59
 
 
 '''
+
+
